71ec073fdce3c809b75fa2ba1971d283c66d48df
maurosantos <santos.mauroantonio@gmail.com>
2016-10-25 10:49:08 -0300

    - Objetos e adaptares no cadastro.

--- app/src/main/java/br/com/maurosantos/android/agendadecontatos/MainActivity.java
+++ app/src/main/java/br/com/maurosantos/android/agendadecontatos/MainActivity.java
@@ -1,14 +1,18 @@
 package br.com.maurosantos.android.agendadecontatos;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageButton btnAdicionar;
+    private EditText edtPesquisa;
+    private ListView lstContatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         btnAdicionar = (ImageButton) findViewById(R.id.btnAdicionar);
+        edtPesquisa = (EditText) findViewById(R.id.edtPesquisa);
+        lstContatos = (ListView) findViewById(R.id.lstContatos);
 
         btnAdicionar.setOnClickListener(this);
     }
--- app/src/main/java/br/com/maurosantos/android/agendadecontatos/NewContactActivity.java
+++ app/src/main/java/br/com/maurosantos/android/agendadecontatos/NewContactActivity.java
@@ -1,13 +1,83 @@
 package br.com.maurosantos.android.agendadecontatos;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class NewContactActivity extends AppCompatActivity {
+
+    private EditText edtNome;
+    private EditText edtTelefone;
+    private EditText edtEmail;
+    private EditText edtEndereco;
+    private EditText edtDatasEspeciais;
+    private EditText edtGrupos;
+
+    private Spinner spnTipoTelefone;
+    private Spinner spnTipoEmail;
+    private Spinner spnTipoEndereco;
+    private Spinner spnTipoDatasEspeciais;
+
+    private ArrayAdapter<String> adpTipoTelefone;
+    private ArrayAdapter<String> adpTipoEmail;
+    private ArrayAdapter<String> adpTipoEndereco;
+    private ArrayAdapter<String> adpTipoDatasEspeciais;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_contact);
+
+        edtNome = (EditText) findViewById(R.id.edtNome);
+        edtTelefone = (EditText) findViewById(R.id.edtTelefone);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtEndereco = (EditText) findViewById(R.id.edtEndereco);
+        edtDatasEspeciais = (EditText) findViewById(R.id.edtDatasEspeciais);
+        edtGrupos = (EditText) findViewById(R.id.edtGrupos);
+
+        spnTipoTelefone = (Spinner) findViewById(R.id.spnTipoTelefone);
+        spnTipoEmail = (Spinner) findViewById(R.id.spnTipoEmail);
+        spnTipoEndereco = (Spinner) findViewById(R.id.spnTipoEndereco);
+        spnTipoDatasEspeciais = (Spinner) findViewById(R.id.spnTipoDatasEspeciais);
+
+        adpTipoTelefone = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpTipoTelefone.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adpTipoEmail = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpTipoEmail.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adpTipoEndereco = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpTipoEndereco.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adpTipoDatasEspeciais = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpTipoDatasEspeciais.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spnTipoTelefone.setAdapter(adpTipoTelefone);
+        spnTipoEmail.setAdapter(adpTipoEmail);
+        spnTipoEndereco.setAdapter(adpTipoEndereco);
+        spnTipoDatasEspeciais.setAdapter(adpTipoDatasEspeciais);
+
+        adpTipoTelefone.add("Casa");
+        adpTipoTelefone.add("Trabalho");
+        adpTipoTelefone.add("Outros");
+
+        adpTipoEmail.add("Celular");
+        adpTipoEmail.add("Trabalho");
+        adpTipoEmail.add("Casa");
+        adpTipoEmail.add("Principal");
+        adpTipoEmail.add("Fax Trabalho");
+        adpTipoEmail.add("Fax Casa");
+        adpTipoEmail.add("Pager");
+        adpTipoEmail.add("Outros");
+
+        adpTipoEndereco.add("Casa");
+        adpTipoEndereco.add("Trabalho");
+        adpTipoEndereco.add("Outros");
+
+        adpTipoDatasEspeciais.add("Aniversário");
+        adpTipoDatasEspeciais.add("Data Comemorativa");
+        adpTipoDatasEspeciais.add("Outros");
     }
 }
